ffc43b95e95c711038a78775dfffc47b457b07ff
Showmen Gupta <shwmndsgpt99@gmail.com>
2024-08-16 12:28:06 +0200

    Fixing the consumer code for show values to consume

--- ApacheKafkaBasics/Services/KafkaConsumerService.cs
+++ ApacheKafkaBasics/Services/KafkaConsumerService.cs
@@ -16,23 +16,24 @@ public class KafkaConsumerService : IKafkaConsumerService
 
     public KafkaConsumerService(string brokerList, string groupId, string topic)
     {
-        var schemaRegistryConfig = new SchemaRegistryConfig { Url = brokerList };
+        var schemaRegistryConfig = new SchemaRegistryConfig { Url = "http://127.0.0.1:8081" };
 
         _consumerConfig = new ConsumerConfig
         {
-            BootstrapServers = "127.0.0.1:9092",
+            BootstrapServers = brokerList,
             GroupId = groupId,
             EnableAutoCommit = false,
             EnableAutoOffsetStore = false,
+            SessionTimeoutMs = 10000, // 10 seconds
             // Read messages from start if no commit exists.
             AutoOffsetReset = AutoOffsetReset.Earliest,
-            MaxPollIntervalMs = 10000
+            MaxPollIntervalMs = 300000
         };
-        var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        var schemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryConfig);
 
         _consumer = new ConsumerBuilder<string, CartItem>(_consumerConfig)
-            .SetKeyDeserializer(new AvroDeserializer<string>(schemaRegistry).AsSyncOverAsync())
-            .SetValueDeserializer(new AvroDeserializer<CartItem>(schemaRegistry).AsSyncOverAsync())
+            .SetKeyDeserializer(new AvroDeserializer<string>(schemaRegistryClient).AsSyncOverAsync())
+            .SetValueDeserializer(new AvroDeserializer<CartItem>(schemaRegistryClient).AsSyncOverAsync())
             .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
             .Build();
 
@@ -43,41 +44,40 @@ public class KafkaConsumerService : IKafkaConsumerService
     public Task StartCartConsumer(CancellationToken cancellationToken)
     {
         var cartItemMessages = new Queue<KafkaMessage>();
-        
+
         if (cartItemMessages == null) throw new BadHttpRequestException("There are no values on the queue");
 
         Console.WriteLine("Consumer loop started...\n");
-        
-        while (true)
-            try
+
+        try
+        {
+            while (!cancellationToken.IsCancellationRequested)
             {
-                var result =
-                    _consumer.Consume(
-                        TimeSpan.FromMilliseconds(_consumerConfig.MaxPollIntervalMs - 1000 ?? 250000));
+                var result = _consumer.Consume(cancellationToken);
                 var cartRequest = result?.Message?.Value;
                 if (cartRequest == null) continue;
 
                 var key = result?.Message?.Key;
                 var partition = result?.Partition.Value;
 
-                // Adding message to a list just for the demo.
-                // You should persist the message in database and process it later.
-                cartItemMessages.Enqueue(new KafkaMessage(key,
-                    partition, cartRequest));
+                cartItemMessages.Enqueue(new KafkaMessage(key, partition, cartRequest));
 
                 _consumer.Commit(result);
                 _consumer.StoreOffset(result);
             }
-            catch (ConsumeException e) when (!e.Error.IsFatal)
-            {
-                Console.WriteLine($"Non fatal error: {e}");
-            }
+        }
+        catch (ConsumeException e) when (!e.Error.IsFatal)
+        {
+            Console.WriteLine($"Non fatal error: {e}");
+        }
+        finally
+        {
+            _consumer.Close();
+        }
 
-            finally
-            {
-                _consumer.Close();
-            }
+        return Task.CompletedTask;
     }
+
 
     public Task<bool> TryDequeueMessage(out string? message)
     {
